2507b427a980836277d5a4900126edb98c9fb251
Jeff Shurak <jshurak@gmail.com>
2014-06-25 14:03:40 -0400

    gm

--- SQLServer_Enumerator.ps1
+++ SQLServer_Enumerator.ps1
@@ -1,1 +1,4 @@
-﻿[System.Data.Sql.SqlDataSourceEnumerator]::Instance.GetDataSources()
+﻿#[System.Data.Sql.SqlDataSourceEnumerator]::Instance.GetDataSources()
+Get-Content ..\..\Clients\AMFC\ServerList.txt | Export-CliXml C:\Users\jshurak\Documents\GitHub\Powershell\servers.xml
+$xml = [xml] (gc C:\Users\jshurak\Documents\GitHub\Powershell\servers.xml)
+$xml.Save([Console]::Out)
