76cd8e8e25d90ecc13e82b4e3de5a04070aadf7d
unknown <jshurak@PHLWL48DB30.kmhp.com>
2014-06-26 15:26:28 -0400

    adding parameters

--- FileCopier.ps1
+++ FileCopier.ps1
@@ -1,2 +1,14 @@
-﻿param([string]$Source,[string]$Destination)
+﻿param(
+    [Parameter(Mandatory=$True)]
+    [ValidateScript({Test-Path $_ -PathType 'Container'})] 
+    [string]$Source,
+    [Parameter(Mandatory=$True)]
+    [ValidateScript({Test-Path $_ -PathType 'Container'})] 
+    [string]$Destination
+    
+    )
 
+
+process{
+
+}
