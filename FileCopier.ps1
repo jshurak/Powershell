f557abc5cf7329d21d20def7487bd9934b9ec5aa
Jeff Shurak <jshurak@gmail.com>
2014-06-26 15:32:51 -0400

    more parameters

--- FileCopier.ps1
+++ FileCopier.ps1
@@ -4,11 +4,15 @@
     [string]$Source,
     [Parameter(Mandatory=$True)]
     [ValidateScript({Test-Path $_ -PathType 'Container'})] 
-    [string]$Destination
+    [string]$Destination,
+    [string]$FileExtension
     
     )
-
+    <#
+    .Synopsis
+        Simple script to recursively copy files from one directory to another
+    #>
 
 process{
-
+    
 }
