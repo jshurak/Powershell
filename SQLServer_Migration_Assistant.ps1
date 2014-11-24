e768314387face47469967360b86b82971dbdcef
jshurak <jshurak@gmail.com>
2014-11-24 14:39:20 -0500

    adding special case for : in file name

--- SQLServer_Migration_Assistant.ps1
+++ SQLServer_Migration_Assistant.ps1
@@ -291,6 +291,7 @@ function script-object {
                 New-Item $Dir -ItemType directory 
             }
             $Name = $_.name -replace '\\',"`$"
+            $Name = $_.name -replace ':',"`$"
             $File = "$Dir" + "\" + $Name + ".sql"
             if(!(Test-Path $File)){
                 $Scriptor.Options.FileName = $File
