c81b5130013e5830637e6c8b1297c8b0138519a8
jshurak <jshurak@gmail.com>
2014-11-24 14:31:11 -0500

    reverting

---
+++ script-amfc.ps1
@@ -0,0 +1,8 @@
+﻿. "C:\Users\jshurak\Desktop\GitHub\kmhp_dms\lib\config.ps1"
+. "C:\Users\jshurak\Desktop\GitHub\Powershell\SQLServer_Migration_Assistant.ps1"
+
+Submit-SQLStatement -ServerInstance $CMSServer -Database $CMSProdDB -Query "SELECT InstanceName FROM MonitoredInstances WHERE Deleted = 0 " | % {
+
+    script-server -InstanceName $_.InstanceName -DestinationRoot "c:\ServerScripts"
+
+}
