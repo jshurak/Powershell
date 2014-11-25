bdcfb5a1947307352cbfd4d48bfdd4444fec9219
Jeff Shurak <jshurak@gmail.com>
2014-11-25 09:32:46 -0500

    updating script-amfc to point to network location

--- script-amfc.ps1
+++ script-amfc.ps1
@@ -3,6 +3,6 @@
 
 Submit-SQLStatement -ServerInstance $CMSServer -Database $CMSProdDB -Query "SELECT InstanceName FROM MonitoredInstances WHERE Deleted = 0 " | % {
 
-    script-server -InstanceName $_.InstanceName -DestinationRoot "c:\ServerScripts"
+    script-server -InstanceName $_.InstanceName -DestinationRoot "\\kmhpemcfspa21\ISTSG\TSG\nsg\Database_Unix_SA\Database Administration Team\MSSQL Admin\Virtualization Effort\ServerScripts"
 
 }
